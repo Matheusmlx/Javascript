072e549c6a6d1d9fc6e65c25936ba264755f8174
matheus.pinho <senha@2019>
2021-04-27 22:30:15 -0300

    Atualizando projeto

---
+++ React/fundamentos-react/src/components/Projeto.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const numbers = [
+    {id:"1",nome:"Matheus",ace:21},
+    {id:"1",nome:"Matheus",ace:21},
+    {id:"1",nome:"Matheus",ace:21},
+    {id:"1",nome:"Matheus",ace:21}
+]
+
+const listItens = numbers.map(pessoa => 
+   <ul>
+       <li>{pessoa.id} | {pessoa.nome} | {pessoa.ace}</li>
+   </ul>
+)
+
+export default () =>
+    <>
+        {listItens}
+        <h2>Exemplo de um componente React</h2>
+    </>
+
+
